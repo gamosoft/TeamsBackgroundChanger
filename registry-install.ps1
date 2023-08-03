ba2bc552fd1913e4280a26e4be8deb81f9e357f9
gamosoft <gamosoft@users.noreply.github.com>
2023-08-03 09:56:19 +0200

    added PS script to handle preview version of Teams

---
+++ registry-install.ps1
@@ -0,0 +1,29 @@
+# Set variables to indicate value and key to set
+$RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Kind.Picture\shell\assign-teams-background'
+$CommandPath  = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Kind.Picture\shell\assign-teams-background\command'
+$Label        = 'Assign as Teams Background'
+$IconKey      = 'Icon'
+$IconValue    = '%USERPROFILE%\AppData\Local\Microsoft\Teams\app.ico'
+############################################
+# Old Teams => C:\Users\[username]\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads
+# $UploadsPath  = '$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads'
+# $FileName     = 'background-changer'
+############################################
+# New Teams => C:\Users\[username]\AppData\Local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads
+$UploadsPath  = '$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads'
+$FileName = '465b6c31-7a35-49fe-9082-54d40489d58b'
+############################################
+$CommandValue = 'PowerShell.exe -NoLogo -NoProfile -WindowStyle Hidden -Command "Copy-Item ''%1'' ' + $UploadsPath + '\' + $FileName + '.png; Copy-Item ''%1'' '+ $UploadsPath +'\' + $FileName + '_thumb.png;"'
+
+# Create the keys if they don't exist
+If (-NOT (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+If (-NOT (Test-Path $CommandPath)) {
+    New-Item -Path $CommandPath -Force | Out-Null
+}
+
+# Now set/update the values
+Set-ItemProperty -Path $RegistryPath -Name '(Default)' -Value $Label -Force
+Set-ItemProperty -Path $RegistryPath -Name $IconKey -Value $IconValue -Force
+Set-ItemProperty -Path $CommandPath -Name '(Default)' -Value $CommandValue -Force
